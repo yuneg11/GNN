474da4e703bdd538121eb5b715a929980fd6b022
EungGu Yun <yuneg11@gmail.com>
2020-09-22 22:51:47 +0900

    Refactor models

--- model.py
+++ model.py
@@ -8,12 +8,12 @@ from layer import (
 )
 
 
-class GCN(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout):
-        super(GCN, self).__init__()
-        self.layer1 = GraphConvolutionLayer(nfeat, nhid, dropout)
+class GCNBase(nn.Module):
+    def __init__(self, ConvolutionLayer, nfeat, nhid, nclass, dropout):
+        super(GCNBase, self).__init__()
+        self.layer1 = ConvolutionLayer(nfeat, nhid, dropout)
         self.relu = nn.ReLU()
-        self.layer2 = GraphConvolutionLayer(nhid, nclass, dropout)
+        self.layer2 = ConvolutionLayer(nhid, nclass, dropout)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, x, adj):
@@ -25,53 +25,54 @@ class GCN(nn.Module):
         return x
 
 
-class GAT(nn.Module):
-    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
-        super(GAT, self).__init__()
-        self.layer1 = GraphAttentionLayer(nfeat, nhid, dropout, alpha, nheads, concat=True)
+class GATBase(nn.Module):
+    def __init__(self, AttentionLayer, nfeat, nhid, nclass, dropout, alpha, nheads):
+        super(GATBase, self).__init__()
+        self.layer1 = AttentionLayer(nfeat, nhid, dropout, alpha, nheads, concat=True)
         self.elu = nn.ELU()
-        self.layer2 = GraphAttentionLayer(nhid * nheads, nclass, dropout, alpha, nheads, concat=False)
+        self.layer2 = AttentionLayer(nhid * nheads, nclass, dropout, alpha, nheads, concat=False)
         self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x, adj):
+        x = self.layer1(x, adj)
+        x = self.elu(x)
+        x = self.layer2(x, adj)
+        if not self.training:
+            x = self.softmax(x)
+        return x
+
+
+class GCN(GCNBase):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__(
+            GraphConvolutionLayer,
+            nfeat, nhid, nclass, dropout
+        )
+
+
+class GAT(GATBase):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+        super(GAT, self).__init__(
+            GraphAttentionLayer,
+            nfeat, nhid, nclass, dropout, alpha, nheads
+        )
 
     def forward(self, x, adj):
         adj = (adj > 0).float()  # Denormalize
-        x = self.layer1(x, adj)
-        x = self.elu(x)
-        x = self.layer2(x, adj)
-        if not self.training:
-            x = self.softmax(x)
-        return x
+        return super().forward(x, adj)
 
 
-class SpGCN(nn.Module):
+class SpGCN(GCNBase):
     def __init__(self, nfeat, nhid, nclass, dropout):
-        super(SpGCN, self).__init__()
-        self.layer1 = SparseGraphConvolutionLayer(nfeat, nhid, dropout)
-        self.relu = nn.ReLU()
-        self.layer2 = SparseGraphConvolutionLayer(nhid, nclass, dropout)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x, adj):
-        x = self.layer1(x, adj)
-        x = self.relu(x)
-        x = self.layer2(x, adj)
-        if not self.training:
-            x = self.softmax(x)
-        return x
+        super(SpGCN, self).__init__(
+            SparseGraphConvolutionLayer,
+            nfeat, nhid, nclass, dropout
+        )
 
 
-class SpGAT(nn.Module):
+class SpGAT(GATBase):
     def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
-        super(SpGAT, self).__init__()
-        self.layer1 = SparseGraphAttentionLayer(nfeat, nhid, dropout, alpha, nheads, concat=True)
-        self.elu = nn.ELU()
-        self.layer2 = SparseGraphAttentionLayer(nhid * nheads, nclass, dropout, alpha, nheads, concat=False)
-        self.softmax = nn.Softmax(dim=1)
-
-    def forward(self, x, adj):
-        x = self.layer1(x, adj)
-        x = self.elu(x)
-        x = self.layer2(x, adj)
-        if not self.training:
-            x = self.softmax(x)
-        return x
+        super(SpGAT, self).__init__(
+            SparseGraphAttentionLayer,
+            nfeat, nhid, nclass, dropout, alpha, nheads
+        )
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import argparse
 from time import time
 
 import numpy as np
+
 import torch
 from torch import optim
 from torch.nn import functional as F
@@ -39,20 +40,20 @@ if __name__ == "__main__":
     # Training settings
     parser = argparse.ArgumentParser()
     parser.add_argument("model", type=str, choices=["gcn", "gat"], help="GCN or GAT")
-    parser.add_argument("--no-cuda", action="store_true", default=False, help="Disable CUDA training.")
-    parser.add_argument("--fastmode", action="store_true", default=False, help="Validate during training pass.")
-    parser.add_argument("--seed", type=int, default=72, help="Random seed.")
-    parser.add_argument("--epochs", type=int, default=1000, help="Number of epochs to train.")
+    parser.add_argument("--no-cuda", action="store_true", default=False, help="Disable CUDA training")
+    parser.add_argument("--fastmode", action="store_true", default=False, help="Validate during training pass")
+    parser.add_argument("--seed", type=int, default=72, help="Random seed")
+    parser.add_argument("--epochs", type=int, default=1000, help="Number of epochs to train")
     parser.add_argument("--save_every", type=int, default=10, help="Save every n epochs")
-    parser.add_argument("--lr", type=float, default=0.005, help="Learning rate.")
-    parser.add_argument("--weight_decay", type=float, default=5e-4, help="Weight decay (L2 loss on parameters).")
-    parser.add_argument("--hidden", type=int, default=8, help="Number of hidden units.")
-    parser.add_argument("--nheads", type=int, default=8, help="Number of head attentions.")
-    parser.add_argument("--dropout", type=float, default=0.6, help="Dropout rate (1 - keep probability).")
-    parser.add_argument("--alpha", type=float, default=0.2, help="Alpha for the leaky_relu.")
-    parser.add_argument("--patience", type=int, default=10, help="Early stopping patience.")
-    parser.add_argument("--dataset", type=str, default="cora", choices=["cora", "citeseer"], help="Dataset to train.")
-    parser.add_argument("--device", type=str, default="cuda", help="Desired device to train.")
+    parser.add_argument("--lr", type=float, default=0.005, help="Learning rate")
+    parser.add_argument("--weight_decay", type=float, default=5e-4, help="Weight decay (L2 loss on parameters)")
+    parser.add_argument("--hidden", type=int, default=8, help="Number of hidden units")
+    parser.add_argument("--nheads", type=int, default=8, help="Number of head attentions")
+    parser.add_argument("--dropout", type=float, default=0.6, help="Dropout rate (1 - keep probability)")
+    parser.add_argument("--alpha", type=float, default=0.2, help="Alpha for the leaky_relu")
+    parser.add_argument("--patience", type=int, default=10, help="Early stopping patience")
+    parser.add_argument("--dataset", type=str, default="cora", choices=["cora", "citeseer"], help="Dataset to train")
+    parser.add_argument("--device", type=str, default="cuda", help="Desired device to train")
     args = parser.parse_args()
 
     args.cuda = not args.no_cuda and torch.cuda.is_available()
